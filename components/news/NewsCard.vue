7907836e2179378827cdc7daa6e901770a89c239
citingdude <glenn.reumers@appwise.be>
2025-01-16 14:54:27 +0100

    Add hover state for news card

--- components/news/NewsCard.vue
+++ components/news/NewsCard.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="flex flex-col md:flex-row">
+  <NuxtLink to="/" class="flex flex-col md:flex-row group/news-card">
     <img src="/images/news/news_1.jpg" alt="">
 
     <div class="bg-light px-9 h-fit pt-40 pb-20">
-      <h3 class="text-[35px] leading-[40px] font-display">
+      <h3
+        class="text-[35px] leading-[40px] font-display group-hover/news-card:text-primary group-hover/news-card:underline transition"
+      >
         Versterk Smart Hub: twee dynamische specialisten gezocht!
       </h3>
 
-      <AppLink class="mt-5" to="/" label="Meer lezen" />
+      <div class="flex w-fit gap-2 items-center mt-5">
+        <span class="text-lg font-display transition group-hover/news-card:text-primary group-hover/news-card:underline">
+          Meer lezen
+        </span>
+
+        <IconArrowRight class="fill-primary" />
+      </div>
     </div>
-  </div>
+  </NuxtLink>
 </template>
