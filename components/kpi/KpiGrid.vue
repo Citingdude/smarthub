af2102ed997e61f519d2b00391245a7fa450d1d5
citingdude <glenn.reumers@appwise.be>
2025-01-16 15:03:46 +0100

    Make kpi card dynamic

--- components/kpi/KpiGrid.vue
+++ components/kpi/KpiGrid.vue
@@ -4,16 +4,28 @@
       src="/images/kpi/kpi_image_1.jpg"
       alt="Robot hond"
     />
-    <KpiGridCard />
+    <KpiGridCard
+      subtitle="Sinds 2000 meer dan"
+      title="135"
+      description="verschillende projecten ondersteund met subsidies"
+    />
     <KpiGridImage
       src="/images/kpi/kpi_image_2.jpg"
       alt="Mensen"
     />
-    <KpiGridCard />
+    <KpiGridCard
+      subtitle="Sinds 2000 meer dan"
+      title="€ 8.000.000"
+      description="aan subsidies uitgekeerd aan innovatieve projecten binnen Vlaams Brabant"
+    />
     <KpiGridImage
       src="/images/kpi/kpi_image_3.jpg"
       alt="Werknemers"
     />
-    <KpiGridCard />
+    <KpiGridCard
+      subtitle="Sinds 2000 genereerden we"
+      title="16.695"
+      description="extra jobs in onze regio"
+    />
   </div>
 </template>
--- components/kpi/KpiGridCard.vue
+++ components/kpi/KpiGridCard.vue
@@ -1,15 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  subtitle: string
+  title: string
+  description: string
+}>()
+</script>
+
 <template>
   <div class="flex flex-col justify-center items-center p-16 gap-4 bg-light aspect-square">
     <p class="text-tertiary text-[20px] leading-[26px] font-light text-center">
-      Sinds 2000 meer dan
+      {{ props.subtitle }}
     </p>
 
     <p class="text-center text-primary font-bold font-display text-[45px] leading-[55px]">
-      135
+      {{ props.title }}
     </p>
 
     <p class="text-tertiary text-[20px] leading-[26px] font-light text-center">
-      verschillende projecten ondersteund met subsidies
+      {{ props.description }}
     </p>
   </div>
 </template>
