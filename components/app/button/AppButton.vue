b43f6654af89eff30b59bc3d24219e036bfa9ff6
citingdude <glenn.reumers@appwise.be>
2025-01-16 14:48:37 +0100

    Fix button styling

--- components/app/button/AppButton.vue
+++ components/app/button/AppButton.vue
@@ -9,29 +9,51 @@ const props = defineProps<{
 }>()
 
 const button = tv({
+  slots: {
+    base: 'py-[14px] px-5 font-bold font-display text-lg rounded-full transition flex items-center gap-[10px] w-full sm:w-fit group',
+    icon: '',
+  },
   base: 'py-[14px] px-5 font-bold font-display text-lg rounded-full transition flex items-center gap-[10px] w-full sm:w-fit',
   variants: {
     color: {
-      white: 'bg-white hover:bg-gradient-to-r hover:from-gradient-from hover:to-gradient-to text-dark fill-primary hover:fill-white hover:text-white',
-      primary: 'bg-gradient-to-r from-gradient-from to-gradient-to text-white',
+      white: {
+        base: 'bg-white hover:bg-gradient-to-r hover:from-gradient-from hover:to-gradient-to text-dark fill-primary hover:fill-white hover:text-white',
+        icon: 'fill-dark group-hover:fill-white',
+      },
+      primary: {
+        base: 'bg-gradient-to-r from-gradient-from to-gradient-to text-white hover:brightness-90',
+        icon: 'fill-white',
+      },
     },
     border: {
-      white: 'border border-white',
-      grey: 'border border-[#C1C1C1]',
+      white: {
+        base: 'border border-white',
+      },
+      grey: {
+        base: 'border border-[#C1C1C1]',
+      },
     },
   },
+})
+
+const {
+  base,
+  icon,
+} = button({
+  color: props.color,
+  border: props.border,
 })
 </script>
 
 <template>
   <NuxtLink
     :to="props.to"
-    :class="button({ color: props.color, border: props.border })"
+    :class="base()"
   >
     <span>
       {{ props.label }}
     </span>
 
-    <IconArrowRight />
+    <IconArrowRight :class="icon()" />
   </NuxtLink>
 </template>
